638e35fc36fbac9413023bf06df0736cc68a2f29
Ishay Junnco <6905@holbertonstudents.com>
2023-11-15 11:05:20 -0800

    next

--- ciclos-immutabilidad/Indexacióndetuplas.py
+++ ciclos-immutabilidad/Indexacióndetuplas.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-#Imprima de manera intercalada todos los elementos de la lista. Ejemplo 1, b, 2, a, 3
+from typing import Tuple
 
-mi_tupla = (1, 2, 3, 'a', 'b')
-print(f"{mi_tupla[0]}, {mi_tupla[3]}, {mi_tupla[1]}, {mi_tupla[4]}, {mi_tupla[2]}")
+mi_tupla = (1, 2, 3, 'a', 'b','c','d',8)
+primer_elemento = mi_tupla[0]  # Acceder al primer elemento
+ultimo_elemento = mi_tupla[-1]  # Acceder al último elemento
+print(f"{mi_tupla[0]}, {mi_tupla[-1]}, {mi_tupla[1]}, {mi_tupla[-2]}, {mi_tupla[2]}, {mi_tupla[-3]}, {mi_tupla[3]}, {mi_tupla[-4]}")
+
+#mi_tupla, primer_elemento, ultimo_elemento
