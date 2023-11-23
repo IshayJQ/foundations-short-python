b231e270c09f7568754ddc292d578f8f77155b04
Ishay Junnco <6905@holbertonstudents.com>
2023-11-23 14:54:16 -0800

    recursion

--- IntroducciónaLaRecursiónEnPython/propiedadDeRecursión:ReducciónDelProblema.py
+++ IntroducciónaLaRecursiónEnPython/propiedadDeRecursión:ReducciónDelProblema.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 from typing import Tuple, Union
 
 def multi_tupla(tupla: Tuple[Union[int, float]]) -> Union[int, float]:
-    if len(tupla) == 0:
+    index = len(tupla)
+    if index == 0:
         return 1
-    return tupla[0] * multi_tupla(tupla[1:])
+    return tupla[index-1] * multi_tupla(tupla[:-1])
 
-tupla =(1,2,3)
-multu = multi_tupla(tupla)
-print(multu)
+tupla = 1,2,3
+multupla = multi_tupla(tupla)
